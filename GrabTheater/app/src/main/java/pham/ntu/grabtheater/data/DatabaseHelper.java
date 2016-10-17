e0eed7a34a8510ce9811ec47fbb0560a58d2b9da
TheAnh <theanh0512@gmail.com>
2016-10-17 18:53:04 +0800

    update databaseHelper

--- GrabTheater/app/src/main/java/pham/ntu/grabtheater/data/DatabaseHelper.java
+++ GrabTheater/app/src/main/java/pham/ntu/grabtheater/data/DatabaseHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import pham.ntu.grabtheater.data.MovieContract.MovieEntry;
+import pham.ntu.grabtheater.data.MovieContract.VideoEntry;
 
 /**
  * Created by Pham on 16/10/2016.
@@ -20,53 +21,54 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-// Create a table to hold locations.  A location consists of the string supplied in the
-        // location setting, the city name, and the latitude and longitude
         final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
-                MovieEntry._ID + " INTEGER PRIMARY KEY," +
-                MovieEntry.COLUMN_MOVIE_ID + " TEXT UNIQUE NOT NULL, " +
-                MovieEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
-                MovieEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
-                MovieEntry.COLUMN_COORD_LONG + " REAL NOT NULL " +
+                MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                MovieEntry.COLUMN_MOVIE_ID + " INTEGER UNIQUE NOT NULL, " +
+                MovieEntry.COLUMN_IS_ADULT + " INTEGER, " +
+                MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_GENRE_IDS + " TEXT " +
+                MovieEntry.COLUMN_ORIGINAL_LANGUAGE + " TEXT " +
+                MovieEntry.COLUMN_ORIGINAL_TITLE + " TEXT " +
+                MovieEntry.COLUMN_OVERVIEW + " TEXT " +
+                MovieEntry.COLUMN_RELEASE_DATE + " TEXT " +
+                MovieEntry.COLUMN_POSTER_PATH + " TEXT " +
+                MovieEntry.COLUMN_POPULARITY + " REAL " +
+                MovieEntry.COLUMN_TITLE + " TEXT " +
+                MovieEntry.COLUMN_HAS_VIDEO + " INTEGER " +
+                MovieEntry.COLUMN_VOTE_AVERAGE + " REAL " +
+                MovieEntry.COLUMN_VOTE_COUNT + " INTEGER " +
                 " );";
 
-        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
-                // Why AutoIncrement here, and not above?
-                // Unique keys will be auto-generated in either case.  But for weather
-                // forecasting, it's reasonable to assume the user will want information
-                // for a certain date and all dates *following*, so the forecast data
-                // should be sorted accordingly.
-                WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-
-                // the ID of the location entry associated with this weather data
-                WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
-                WeatherEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
-                WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
-                WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL," +
-
-                WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
-
-                WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
+        final String SQL_CREATE_VIDEO_TABLE = "CREATE TABLE " + VideoEntry.TABLE_NAME + " (" +
+                VideoEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                //id associated with movie
+                VideoEntry.COLUMN_MOVIE_KEY + " INTEGER NOT NULL, " +
+                VideoEntry.COLUMN_VIDEO_ID + " TEXT NOT NULL, " +
+                VideoEntry.COLUMN_ISO + " TEXT NOT NULL, " +
+                VideoEntry.COLUMN_VIDEO_KEY + " TEXT NOT NULL," +
+                VideoEntry.COLUMN__VIDEO_NAME + " TEXT NOT NULL, " +
+                VideoEntry.COLUMN_VIDEO_SITE + " TEXT NOT NULL, " +
+                VideoEntry.COLUMN_VIDEO_SIZE + " INT NOT NULL, " +
+                VideoEntry.COLUMN_VIDEO_TYPE + " TEXT NOT NULL, " +
 
                 // Set up the location column as a foreign key to location table.
-                " FOREIGN KEY (" + WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " +
-                LocationEntry.TABLE_NAME + " (" + LocationEntry._ID + "), " +
+                " FOREIGN KEY (" + VideoEntry.COLUMN_MOVIE_KEY + ") REFERENCES " +
+                MovieEntry.TABLE_NAME + " (" + MovieEntry._ID + ");";
 
-                // To assure the application have just one weather entry per day
-                // per location, it's created a UNIQUE constraint with REPLACE strategy
-                " UNIQUE (" + WeatherEntry.COLUMN_DATE + ", " +
-                WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
-
-        sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
-        sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_MOVIE_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_VIDEO_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        // Note that this only fires if you change the version number for your database.
+        // It does NOT depend on the version number for your application.
+        // If you want to update the schema without wiping data, commenting out the next 2 lines
+        // should be your top priority before modifying this method.
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + VideoEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 }
